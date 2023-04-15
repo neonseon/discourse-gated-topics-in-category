49b7048b3e70682bcbbdfceb790f7e76fa72e0ca
Arkshine <github@arkshine.me>
2023-04-15 14:15:36 +0200

    FEATURE: Add separate categories/tags for logged-in users

--- javascripts/discourse/components/topic-in-gated-category.js
+++ javascripts/discourse/components/topic-in-gated-category.js
@@ -1,13 +1,24 @@
 import Component from "@ember/component";
 import discourseComputed from "discourse-common/utils/decorators";
 
-const enabledCategories = settings.enabled_categories
-  .split("|")
-  .map((id) => parseInt(id, 10))
-  .filter((id) => id);
+const parserInt = (setting) => {
+  return setting
+    .split("|")
+    .map((id) => parseInt(id, 10))
+    .filter((id) => id);
+};
 
-const enabledTags = settings.enabled_tags.split("|").filter(Boolean);
-const groups = settings.allowed_groups.split("|").map((id) => parseInt(id, 10));
+const parserBool = (setting) => {
+  return setting.split("|").filter(Boolean);
+};
+
+const enabledCategoriesAnonymous = parserInt(settings.enabled_categories);
+const enabledCategoriesUser = parserInt(settings.enabled_categories_user);
+
+const enabledTagsAnonymous = parserBool(settings.enabled_tags);
+const enabledTagsUser = parserBool(settings.enabled_tags_user);
+
+const groups = parserInt(settings.allowed_groups);
 
 export default Component.extend({
   tagName: "",
@@ -28,16 +39,23 @@ export default Component.extend({
   },
 
   recalculate() {
+    const enabledTags = this.currentUser
+      ? enabledTagsUser
+      : enabledTagsAnonymous;
+    const enabledCategories = this.currentUser
+      ? enabledCategoriesUser
+      : enabledCategoriesAnonymous;
+    const gatedByTag = this.tags?.some((t) => enabledTags.includes(t));
+
     // do nothing if:
     // a) topic does not have a category and does not have a gated tag
     // b) component setting is empty
-    // c) user is logged in
-    const gatedByTag = this.tags?.some((t) => enabledTags.includes(t));
-
+    // c) user is logged in and is in a group that is allowed to see the topic
     if (
       (!this.categoryId && !gatedByTag) ||
       (enabledCategories.length === 0 && enabledTags.length === 0) ||
-      (this.currentUser && this.currentUser.groups?.some((g) => groups.includes(g.id)))
+      (this.currentUser &&
+        this.currentUser.groups?.some((g) => groups.includes(g.id)))
     ) {
       return;
     }
@@ -51,5 +69,5 @@ export default Component.extend({
   @discourseComputed("hidden")
   shouldShow(hidden) {
     return !hidden;
-  }
+  },
 });
