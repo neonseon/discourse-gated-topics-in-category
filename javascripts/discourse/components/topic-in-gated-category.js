fd893177c381d0e6ad8feedbecdad4f42c719f7d
Seon <sar@biotest.net>
2023-04-17 11:40:01 -0700

    Revert "FIX: Remove original settings and make sure template is not overwritten"
    This reverts commit 2c4e87122abc2251252ea7ed790a71b3fcaaada6.

--- javascripts/discourse/components/topic-in-gated-category.js
+++ javascripts/discourse/components/topic-in-gated-category.js
@@ -1,7 +1,11 @@
 import Component from "@ember/component";
 import discourseComputed from "discourse-common/utils/decorators";
 
-let enabledCategoriesUser, enabledTagsUser, allowedGroups;
+let enabledCategoriesAnonymous,
+  enabledCategoriesUser,
+  enabledTagsAnonymous,
+  enabledTagsUser,
+  allowedGroups;
 
 function parserInt(setting) {
   return setting
@@ -14,14 +18,16 @@ function parserBool(setting) {
   return setting.split("|").filter(Boolean);
 }
 function refreshSettings() {
+  enabledCategoriesAnonymous = parserInt(settings.enabled_categories);
   enabledCategoriesUser = parserInt(settings.enabled_categories_user);
+  enabledTagsAnonymous = parserBool(settings.enabled_tags);
   enabledTagsUser = parserBool(settings.enabled_tags_user);
   allowedGroups = parserInt(settings.allowed_groups);
 }
 
 export default Component.extend({
   tagName: "",
-  hidden_user: true,
+  hidden: true,
 
   didInsertElement() {
     this._super(...arguments);
@@ -39,30 +45,35 @@ export default Component.extend({
   },
 
   recalculate() {
-    const gatedByTag = this.tags?.some((t) => enabledTagsUser.includes(t));
+    const enabledTags = this.currentUser
+      ? enabledTagsUser
+      : enabledTagsAnonymous;
+    const enabledCategories = this.currentUser
+      ? enabledCategoriesUser
+      : enabledCategoriesAnonymous;
+    const gatedByTag = this.tags?.some((t) => enabledTags.includes(t));
 
     // do nothing if:
     // a) topic does not have a category and does not have a gated tag
     // b) component setting is empty
-    // c) user is not logged in
-    // d) user is logged in and is in a group that is allowed to see the topic
+    // c) user is logged in and is in a group that is allowed to see the topic
     if (
       (!this.categoryId && !gatedByTag) ||
-      (enabledCategoriesUser.length === 0 && enabledTagsUser.length === 0) ||
-      !this.currentUser ||
-      this.currentUser.groups?.some((g) => allowedGroups.includes(g.id))
+      (enabledCategories.length === 0 && enabledTags.length === 0) ||
+      (this.currentUser &&
+        this.currentUser.groups?.some((g) => allowedGroups.includes(g.id)))
     ) {
       return;
     }
 
-    if (enabledCategoriesUser.includes(this.categoryId) || gatedByTag) {
+    if (enabledCategories.includes(this.categoryId) || gatedByTag) {
       document.body.classList.add("topic-in-gated-category");
-      this.set("hidden_user", false);
+      this.set("hidden", false);
     }
   },
 
-  @discourseComputed("hidden_user")
-  shouldShow(hidden_user) {
-    return !hidden_user;
+  @discourseComputed("hidden")
+  shouldShow(hidden) {
+    return !hidden;
   },
 });
