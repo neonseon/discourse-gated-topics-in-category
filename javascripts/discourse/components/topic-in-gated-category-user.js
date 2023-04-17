2c4e87122abc2251252ea7ed790a71b3fcaaada6
Arkshine <github@arkshine.me>
2023-04-17 19:27:24 +0200

    FIX: Remove original settings and make sure template is not overwritten

--- javascripts/discourse/components/topic-in-gated-category-user.js
+++ javascripts/discourse/components/topic-in-gated-category-user.js
@@ -1,11 +1,7 @@
 import Component from "@ember/component";
 import discourseComputed from "discourse-common/utils/decorators";
 
-let enabledCategoriesAnonymous,
-  enabledCategoriesUser,
-  enabledTagsAnonymous,
-  enabledTagsUser,
-  allowedGroups;
+let enabledCategoriesUser, enabledTagsUser, allowedGroups;
 
 function parserInt(setting) {
   return setting
@@ -18,16 +14,14 @@ function parserBool(setting) {
   return setting.split("|").filter(Boolean);
 }
 function refreshSettings() {
-  enabledCategoriesAnonymous = parserInt(settings.enabled_categories);
   enabledCategoriesUser = parserInt(settings.enabled_categories_user);
-  enabledTagsAnonymous = parserBool(settings.enabled_tags);
   enabledTagsUser = parserBool(settings.enabled_tags_user);
   allowedGroups = parserInt(settings.allowed_groups);
 }
 
 export default Component.extend({
   tagName: "",
-  hidden: true,
+  hidden_user: true,
 
   didInsertElement() {
     this._super(...arguments);
@@ -45,35 +39,30 @@ export default Component.extend({
   },
 
   recalculate() {
-    const enabledTags = this.currentUser
-      ? enabledTagsUser
-      : enabledTagsAnonymous;
-    const enabledCategories = this.currentUser
-      ? enabledCategoriesUser
-      : enabledCategoriesAnonymous;
-    const gatedByTag = this.tags?.some((t) => enabledTags.includes(t));
+    const gatedByTag = this.tags?.some((t) => enabledTagsUser.includes(t));
 
     // do nothing if:
     // a) topic does not have a category and does not have a gated tag
     // b) component setting is empty
-    // c) user is logged in and is in a group that is allowed to see the topic
+    // c) user is not logged in
+    // d) user is logged in and is in a group that is allowed to see the topic
     if (
       (!this.categoryId && !gatedByTag) ||
-      (enabledCategories.length === 0 && enabledTags.length === 0) ||
-      (this.currentUser &&
-        this.currentUser.groups?.some((g) => allowedGroups.includes(g.id)))
+      (enabledCategoriesUser.length === 0 && enabledTagsUser.length === 0) ||
+      !this.currentUser ||
+      this.currentUser.groups?.some((g) => allowedGroups.includes(g.id))
     ) {
       return;
     }
 
-    if (enabledCategories.includes(this.categoryId) || gatedByTag) {
+    if (enabledCategoriesUser.includes(this.categoryId) || gatedByTag) {
       document.body.classList.add("topic-in-gated-category");
-      this.set("hidden", false);
+      this.set("hidden_user", false);
     }
   },
 
-  @discourseComputed("hidden")
-  shouldShow(hidden) {
-    return !hidden;
+  @discourseComputed("hidden_user")
+  shouldShow(hidden_user) {
+    return !hidden_user;
   },
 });
--- test/acceptance/gated-category-test.js
+++ test/acceptance/gated-category-test.js
@@ -17,7 +17,7 @@ acceptance("Gated Topics - Anonymous", function (needs) {
   test("Viewing Topic in gated category", async function (assert) {
     await visit("/t/internationalization-localization/280");
 
-    assert.ok(
+    assert.notOk(
       query(".topic-in-gated-category .custom-gated-topic-content"),
       "gated category prompt shown for anons on selected category"
     );
@@ -35,41 +35,9 @@ acceptance("Gated Topics - Anonymous", function (needs) {
   test("Viewing Topic with gated tag", async function (assert) {
     await visit("/t/2480");
 
-    assert.ok(
+    assert.notOk(
       query(".topic-in-gated-category .custom-gated-topic-content"),
       "gated category prompt shown for anons on topic with selected tag"
-    );
-  });
-});
-
-acceptance("Gated Topics - Logged In - Anonymous Settings", function (needs) {
-  needs.user();
-  needs.settings({ tagging_enabled: true });
-  needs.hooks.beforeEach(() => {
-    settings.enabled_categories = "2";
-    settings.enabled_tags = "foo|baz";
-  });
-
-  needs.hooks.afterEach(() => {
-    settings.enabled_categories = "";
-    settings.enabled_tags = "";
-  });
-
-  test("Viewing Topic in gated category with anonymous settings", async function (assert) {
-    await visit("/t/internationalization-localization/280");
-
-    assert.notOk(
-      query(".topic-in-gated-category .custom-gated-topic-content"),
-      "gated category prompt not shown on selected category with anonymous settings"
-    );
-  });
-
-  test("Viewing Topic with gated tag with anonymous settings", async function (assert) {
-    await visit("/t/2480");
-
-    assert.notOk(
-      query(".topic-in-gated-category .custom-gated-topic-content"),
-      "gated category prompt not shown on topic with selected tag with anonymous settings"
     );
   });
 });
